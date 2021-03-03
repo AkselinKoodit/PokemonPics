e12201a0d1398e3a75bfcffd7f8cbcd94718242f
Akseli <s2100140@edu.bc.fi>
2021-03-03 22:39:45 +0200

    cleaned a little

--- script.js
+++ script.js
@@ -19,59 +19,38 @@ function addPokemon(pokeObjList) {
   let newLi = document.createElement("li");
   newLi.innerText = `${pokeObjList.name}`;
   document.getElementById("pokeList").appendChild(newLi);
-
-  // let newRow = document.createElement("tr");
-  // newRow.innerHTML = `
-  //   <td>${pokeObj.name}</td>
-  //   <td> <a href = "link">${pokeObj.url}</a></td>`;
-  // document.getElementById("table-content").appendChild(newRow);
 }
 
 document.getElementById("pica").addEventListener(
   "click",
   (el = () => {
-    let imageAnchor;
     fetch("https://pokeapi.co/api/v2/pokemon/172/")
       .then((response) => response.json())
       .then((json) => {
         let picachu = json;
         console.log(picachu.sprites.front_default);
-        showInfo(picachu);
-
-        // "Hopefully here's the image link: " +
-        //   json.results.sprites.front_default
+        showInfo();
       });
   })
 );
 
-function showInfo(pokeObj) {
-  let infoArea = document.getElementById("infoboard");
-  let newDiv = document.createElement("div");
-  let pokeArray = [];
-  let abilities = pokeObj.abilities;
-  let baseExperience = pokeObj.base_experience;
-  let forms = [];
-  let weight = pokeObj.weight;
-  let spritesList = [];
+function showInfo() {
+  console.log("Fn started");
+  // let infoArea = document.getElementById("infoboard");
+  // let newDiv = document.createElement("div");
+  // let pokeArray = [];
+  // let abilities = pokeObj.abilities;
+  // let baseExperience = pokeObj.base_experience;
+  // let forms = [];
+  // let weight = pokeObj.weight;
+  // let spritesList = [];
 
-  pokeObj.forms.forEach((el) => {
-    forms.push(el);
-  });
-  pokeObj.forms.forEach((el) => {
-    spritesList.push(el.sprites);
-  });
-
-  console.log(pokeObj);
-  console.log("Picachu has following attributes: ");
-  console.log("Abilities: " + abilities[0].value);
-  console.log("Weight: " + weight);
-  console.log("forms: " + forms);
-  console.log("Base-experience: " + baseExperience);
-  console.log("Url for images: " + spritesList);
-
-  newDiv.innerText = `${pokeObj.name}`;
-  infoArea.appendChild(newDiv);
-  infoArea.classList.add("image");
+  // pokeObj.forms.forEach((el) => {
+  //   forms.push(el);
+  // });
+  // pokeObj.forms.forEach((el) => {
+  //   spritesList.push(el.sprites);
+  // });
 
   let pokemonBtns = document.querySelectorAll("li");
 
@@ -82,6 +61,7 @@ function showInfo(pokeObj) {
   }
 
   function clicked(index) {
+    document.getElementById("infoboard").innerHTML = "";
     console.log("You clicked " + pokemons[index].name);
     console.log("It's url: " + pokemons[index].url);
 
@@ -92,7 +72,7 @@ function showInfo(pokeObj) {
         console.log(pokemon.sprites.front_default);
 
         let imageDiv = document.createElement("div");
-        imageDiv.innerHTML = `<div>Pokemon info: Weight: ${pokemon.weight}<img src="${pokemon.sprites.front_default}" alt="eweq"></img></div>;`;
+        imageDiv.innerHTML = `<div>Pokemon info: Name: ${pokemons[index].name} Weight: ${pokemon.weight}<img src="${pokemon.sprites.front_default}"alt = "eweq"></img></div>`;
         document.getElementById("infoboard").appendChild(imageDiv);
       });
   }
